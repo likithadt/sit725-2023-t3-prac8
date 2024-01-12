3bea4e819dbfff0fe1c922c55c22a63a26ea87ba
likithadt <49176216+likithadt@users.noreply.github.com>
2024-01-12 11:30:27 +1100

    Dockerised-prac3-project

---
+++ public/scripts/script.js
@@ -0,0 +1,48 @@
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "images/kitten2.jpg",
+        link: "About Kitten 2",
+        description: "Meow there!Paw-sitively thrilled to be communicating with you!"
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten3.jpg",
+        link: "About Kitten 3",
+        description: "Sending whisker kisses and playful paw waves your way! Hope your day is filled with sunshine and treats galore!"
+    }
+];
+
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+const submitForm = () => {
+    let formData = {};
+    formData.first_name = $('#first_name').val();
+    formData.last_name = $('#last_name').val();
+    formData.password = $('#password').val();
+    formData.email = $('#email').val();
+    console.log("Form Data Submitted:", formData);
+    alert("Form Data Submitted successfully");
+}
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.description + '</p>' +
+            '</div></div></div>';
+        $("#card-section").append(itemToAppend)
+    });
+}
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(() => {
+        submitForm();
+    })
+    addCards(cardList);
+    $('.modal').modal();
+});
